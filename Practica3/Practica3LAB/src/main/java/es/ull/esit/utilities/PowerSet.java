78137fa1fa1ebb01fb26a61403c313f7931a84a3
Ruymán García Martín <alu0101408866@ull.edu.es>
2024-11-02 14:04:23 +0000

    Arreglando el problema de los multiples recorridos con el nuevo iterador

--- Practica3/Practica3LAB/src/main/java/es/ull/esit/utilities/PowerSet.java
+++ Practica3/Practica3LAB/src/main/java/es/ull/esit/utilities/PowerSet.java
@@ -2,58 +2,63 @@ package es.ull.esit.utilities;
 
 import java.util.BitSet;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.Set;
 import java.util.TreeSet;
-import java.util.NoSuchElementException;
-
 
 // Sirve para calcular todos los subconjuntos de un conjunto dado
-public class PowerSet<E> implements Iterator<Set<E>>, Iterable<Set<E>> {
+public class PowerSet<E> implements Iterable<Set<E>> {
 
-    private E[] arr = null;
-    private BitSet bset = null;
+    private E[] arr;
 
     @SuppressWarnings("unchecked")
     public PowerSet(Set<E> set) {
         this.arr = (E[]) set.toArray();
-        this.bset = new BitSet(this.arr.length + 1);
-    }
-
-    @Override
-    public boolean hasNext() {
-        return !this.bset.get(this.arr.length);
-    }
-
-    @Override
-    public Set<E> next() {
-        if (!hasNext()) {
-            throw new NoSuchElementException("No more elements in the PowerSet.");
-        }
-
-        Set<E> returnSet = new TreeSet<>();
-        for (int i = 0; i < this.arr.length; i++) {
-            if (this.bset.get(i)) {
-                returnSet.add(this.arr[i]);
-            }
-        }
-        for (int i = 0; i < this.bset.size(); i++) {
-            if (!this.bset.get(i)) {
-                this.bset.set(i);
-                break;
-            } else {
-                this.bset.clear(i);
-            }
-        }
-        return returnSet;
-    }
-
-    @Override
-    public void remove() {
-        throw new UnsupportedOperationException("Not Supported!");
     }
 
     @Override
     public Iterator<Set<E>> iterator() {
-        return this;
+        return new PowerSetIterator();
+    }
+
+    private class PowerSetIterator implements Iterator<Set<E>> {
+        private BitSet bset;
+
+        public PowerSetIterator() {
+            this.bset = new BitSet(arr.length + 1);
+        }
+
+        @Override
+        public boolean hasNext() {
+            return !this.bset.get(arr.length);
+        }
+
+        @Override
+        public Set<E> next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException("No more elements in the PowerSet.");
+            }
+
+            Set<E> returnSet = new TreeSet<>();
+            for (int i = 0; i < arr.length; i++) {
+                if (this.bset.get(i)) {
+                    returnSet.add(arr[i]);
+                }
+            }
+            for (int i = 0; i < this.bset.size(); i++) {
+                if (!this.bset.get(i)) {
+                    this.bset.set(i);
+                    break;
+                } else {
+                    this.bset.clear(i);
+                }
+            }
+            return returnSet;
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException("Not Supported!");
+        }
     }
 }
